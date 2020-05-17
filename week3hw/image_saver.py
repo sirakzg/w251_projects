3acc880780bd5f30216219a23b18568c8bd85468
sirakzg <sirakzg@berkeley.edu>
2020-05-16 21:04:14 -0600

    Adding first batch of files.

---
+++ week3hw/image_saver.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import time
+import cv2
+import numpy as np
+
+import paho.mqtt.client as mqtt
+
+if len(sys.argv) != 2 or not os.path.isdir(sys.argv[1]):
+	print("Help: run command as $>image_saver.py <output folder>")
+	exit()
+
+
+LOCAL_MQTT_HOST="localhost"
+LOCAL_MQTT_PORT=1883
+LOCAL_MQTT_TOPIC="face_detect"
+
+def on_connect_local(client, userdata, flags, rc):
+	print("connected to local broker with rc: " + str(rc))
+	client.subscribe(LOCAL_MQTT_TOPIC)
+	
+def on_message(client,userdata, msg):
+	try:
+		print("message received!", int(time.time()) )	
+		# if we wanted to re-publish this message, something like this should work
+		# msg = msg.payload
+		# remote_mqttclient.publish(REMOTE_MQTT_TOPIC, payload=msg, qos=0, retain=False)
+
+		nparr = np.fromstring(msg.payload, np.uint8)
+		img = cv2.imdecode(nparr,  cv2.IMREAD_GRAYSCALE)
+
+		# opencv to save to file
+		cv2.imwrite( os.path.join(sys.argv[1], "face_"+str(int(time.time()))+".png"), img)
+
+	except:
+		print("Unexpected error:", sys.exc_info()[0])
+
+local_mqttclient = mqtt.Client()
+local_mqttclient.on_connect = on_connect_local
+local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
+local_mqttclient.on_message = on_message
+
+
+
+# go into a loop
+local_mqttclient.loop_forever()
---
+++ week3hw/opencv_faceDetector.py
@@ -0,0 +1,66 @@
+import numpy as np
+import cv2
+import time
+
+import paho.mqtt.client as mqtt
+
+# load the OpenCV face classifier
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# 1 should correspond to /dev/video1 , your USB camera. The 0 is reserved for the TX2 onboard camera
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+cap.set(cv2.CAP_PROP_FPS, 12)
+
+
+
+LOCAL_MQTT_HOST="localhost"
+LOCAL_MQTT_PORT=1883
+LOCAL_MQTT_TOPIC="face_detect"
+
+def on_connect_local(client, userdata, flags, rc):
+        print("connected to local broker with rc: " + str(rc))
+        client.subscribe(LOCAL_MQTT_TOPIC)
+	
+
+client = mqtt.Client()
+client.on_connect = on_connect_local
+client.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_PORT, 60)
+
+
+while(True):
+	# Capture frame-by-frame
+	ret, frame = cap.read()
+
+	# We don't use the color information, so might as well save space
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# face detection and other logic goes here
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	for (x,y,w,h) in faces:
+		# your logic goes here; for instance
+		# cut out face from the frame..
+		# rc,png = cv2.imencode('.png', face)
+		# msg = png.tobytes()
+		# ...
+		#print("Face: ",x,y,w,h)
+
+		# publish face!
+		crop = cv2.imencode(".png", gray[y:y+h,x:x+w])[1].tobytes()
+		client.publish("face_detect", crop, qos=1)
+
+		# draw on gray image for imshow
+		gray = cv2.rectangle(gray, (x,y), (x+w,y+h), (0,0,0), 2)
+
+
+	# Display the resulting frame
+	cv2.imshow('frame',gray)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+	   break
+
+	time.sleep(1)
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
