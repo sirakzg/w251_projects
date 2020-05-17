3f89874bec1ace1ff9ab033ef6fdc5dd649d1716
sirakzg <sirakzg@berkeley.edu>
2020-05-17 14:55:16 -0600

    corrections to config files, added opencv container command

--- week3hw/run_cloud_mqtts.sh
+++ week3hw/run_cloud_mqtts.sh
@@ -1,2 +1,2 @@
 docker run --name cloud-broker -p 1883:1883 --network face-bridge -v ~/w251_projects/week3hw:/w251 --env MQTT_CONFIG='/w251/cloud_broker.conf' --rm -d mqtt_broker
-docker run --name image-saver --network face-brid --env SAVE_FOLDER='/ibmcloud' --rm -d -v /mnt/ibm-objstore:/ibmcloud image-saver
+docker run --name image-saver --network face-brid --env SAVE_FOLDER='/ibmcloud' --rm -d -v /mnt/ibm-objstore:/ibmcloud/w251 image-saver
--- week3hw/run_jtx2_mqtts.sh
+++ week3hw/run_jtx2_mqtts.sh
@@ -1,3 +1,4 @@
+sudo docker run --name face-detector -e DISPLAY=$DISPLAY --privileged --network face-bridge --env QT_X11_NO_MITSHM=1 --rm -d face-detect
 sudo docker run --name mqtt_broker1 --network face-bridge -v ~/w251_projects/week3hw/:/w251 --env MQTT_CONFIG='/w251/jtx2_broker.conf' --rm -d mqtt_broker
 sudo docker run --name jtx2_forwarder --network face-bridge -v ~/w251_projects/week3hw/:/w251 --env MQTT_CONFIG='/w251/jtx2_forwarder.conf' --rm -d mqtt_broker
 
