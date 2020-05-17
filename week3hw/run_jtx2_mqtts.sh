1daee001d15eb5de8c30a1e45b5873cb6e793bf3
sirakzg <sirakzg@berkeley.edu>
2020-05-17 12:16:35 -0600

    adding verbose flag to mqtt container, additional flags to run command

--- week3hw/run_jtx2_mqtts.sh
+++ week3hw/run_jtx2_mqtts.sh
@@ -1,3 +1,3 @@
-sudo docker run --name mqtt_broker1 --network face-bridge -v ~/w251_projects/week3hw/:/w251 --env MQTT_CONFIG='/w251/jtx2_broker.conf' -d mqtt_broker
-sudo docker run --name mqtt_forwarder --network face-bridge -v ~/w251_projects/week3hw/:/w251 --env MQTT_CONFIG='/w251/jtx2_forwarder.conf' -d mqtt_broker
+sudo docker run --name mqtt_broker1 --network face-bridge -v ~/w251_projects/week3hw/:/w251 --env MQTT_CONFIG='/w251/jtx2_broker.conf' --rm -d mqtt_broker
+sudo docker run --name jtx2_forwarder --network face-bridge -v ~/w251_projects/week3hw/:/w251 --env MQTT_CONFIG='/w251/jtx2_forwarder.conf' --rm -d mqtt_broker
 
